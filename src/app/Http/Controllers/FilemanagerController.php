388a663cffd881453335ba8513f51a960efc9e33
Roman Zakharov <camanru@ya.ru>
2019-05-11 16:20:53 +0300

    1.0.1

--- src/app/Http/Controllers/FilemanagerController.php
+++ src/app/Http/Controllers/FilemanagerController.php
@@ -5,14 +5,24 @@ namespace Fastleo\Fastleo;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\ImageManagerStatic as Image;
 
 class FilemanagerController extends Controller
 {
     public $images = ['png', 'jpg', 'jpeg', 'gif'];
 
+    public $storage = 'public/uploads';
+
     public $upload = '/storage/uploads';
 
-    public $dir, $folders, $files;
+    public $folders;
+
+    public $folder;
+
+    public $files;
+
+    public $patn;
 
     /**
      * FilemanagerController constructor.
@@ -20,142 +30,45 @@ class FilemanagerController extends Controller
      */
     public function __construct(Request $request)
     {
-        // upload folder name
-        $this->dir = base_path('storage/app/public/uploads');
+        // Основная директорая
+        $this->path = base_path('storage/app/' . $this->storage);
+        $this->folder = '';
 
-        // Create uploads dir
-        if (!is_dir($this->dir)) {
-            File::makeDirectory($this->dir, $mode = 0777, true, true);
+        // Создание основной директории
+        if (!is_dir($this->path)) {
+            File::makeDirectory($this->full_path, $mode = 0777, true, true);
         }
 
-        // Change dir
-        if ($request->get('folder')) {
-            if ($request->get('folder') == '/') {
-                $request->merge(['folder' => '']);
-            }
-            $this->dir = base_path('storage/app/public/uploads/' . $request->get('folder'));
-            $this->upload = $this->upload .'/'. $request->get('folder');
+        // Создание папки со зжатыми изображениями
+        if (!is_dir($this->path . '/.thumbs')) {
+            File::makeDirectory($this->path . '/.thumbs', $mode = 0777, true, true);
         }
 
-        // create thumbs folder
-        if (!is_dir($this->dir . '/.thumbs')) {
-            File::makeDirectory($this->dir . '/.thumbs', $mode = 0777, true, true);
-        }
+        // Все папки в директории
+        $this->folders = collect(Storage::directories('public/uploads'))->except($this->storage . '/thumbs');
+
+        // Все файлы в директории
+        $this->files = collect(Storage::files($this->storage));
     }
 
     /**
-     * @param $path
-     * @param $image
-     * @param int $desired_width
+     * Данные о расширении файла
+     * @param $filename
+     * @return mixed
+     */
+    protected function getExtention($filename)
+    {
+        return pathinfo($this->dir . '/' . $filename, PATHINFO_EXTENSION);
+    }
+
+    /**
+     * Проверка на изображение
+     * @param $extention
      * @return bool
      */
-    private function resize($path, $image, $desired_width = 122)
+    protected function checkImage($extention)
     {
-        $mime_type = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $this->dir . '/' . $image);
-        $mime_array = explode("/", $mime_type);
-        $ext = end($mime_array);
-
-        switch ($ext) {
-            case 'png':
-                $source_image = @imagecreatefrompng($path . '/' . $image);
-                break;
-            case 'jpg':
-                $source_image = @imagecreatefromjpeg($path . '/' . $image);
-                break;
-            case 'jpeg':
-                $source_image = @imagecreatefromjpeg($path . '/' . $image);
-                break;
-            case 'gif':
-                $source_image = @imagecreatefromgif($path . '/' . $image);
-                break;
-        }
-
-        if (!$source_image) {
-            return false;
-        }
-
-        $width = imagesx($source_image);
-        $height = imagesy($source_image);
-        $desired_height = floor($height * ($desired_width / $width));
-        $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
-        imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
-
-        switch ($ext) {
-            case 'png':
-                imagepng($virtual_image, $path . '/.thumbs/' . $image);
-                break;
-            case 'jpg':
-                imagejpeg($virtual_image, $path . '/.thumbs/' . $image);
-                break;
-            case 'jpeg':
-                imagejpeg($virtual_image, $path . '/.thumbs/' . $image);
-                break;
-            case 'gif':
-                imagegif($virtual_image, $path . '/.thumbs/' . $image);
-                break;
-        }
-
-        return $this->upload . '/.thumbs/' . $image;
-    }
-
-    /**
-     * Folders list
-     * @param Request $request
-     * @param array $folders
-     * @return array
-     */
-    protected function getFolders($request, $folders = [])
-    {
-
-        // Redirect to folder
-        if (!$request->has('folder')) {
-            header('Location: /fastleo/filemanager?' . request()->getQueryString() . '&folder=' . $request->session()->get('folder'));
-            die;
-        }
-
-        // save current folder
-        $request->session()->put('folder', $request->get('folder'));
-        $request->session()->save();
-
-        // folders list
-        $scan = scandir($this->dir);
-        if (count($scan) > 0) {
-            foreach ($scan as $e) {
-                if (!in_array($e, ['.', '..', '.thumbs']) and is_dir($this->dir . '/' . $e)) {
-                    $folders[] = $e;
-                }
-            }
-        }
-        return $folders;
-    }
-
-    /**
-     * Files list
-     * @param array $files
-     * @return array
-     */
-    protected function getFiles($files = [])
-    {
-        $scan = scandir($this->dir);
-        if (count($scan) > 0) {
-            foreach ($scan as $e) {
-                if (!in_array($e, ['.', '..', '.thumbs']) and is_file($this->dir . '/' . $e)) {
-                    $ext = pathinfo($this->dir . '/' . $e, PATHINFO_EXTENSION);
-                    if (!file_exists($this->dir . '/.thumbs' . $e) and in_array(strtolower($ext), $this->images)) {
-                        $thumbs = self::resize($this->dir, $e);
-                    } else {
-                        $thumbs = (file_exists($this->dir . '/.thumbs' . $e)) ? $this->upload . '/.thumbs' . $e : '';
-                    }
-                    $files[] = [
-                        'name' => $e,
-                        'url' => $this->upload . '/' . $e,
-                        'thumbs' => $thumbs,
-                        'ext' => $ext,
-                    ];
-                }
-            }
-        }
-        return $files;
+        return (in_array(strtolower($ext), $this->images)) ? true : false;
     }
 
     /**
@@ -166,9 +79,9 @@ class FilemanagerController extends Controller
     public function index(Request $request)
     {
         return view('fastleo::filemanager/index', [
-            'folders' => self::getFolders($request),
-            'files' => self::getFiles(),
+            'folders' => $this->folders,
             'images' => $this->images,
+            'files' => $this->files,
         ]);
     }
 
@@ -196,6 +109,7 @@ class FilemanagerController extends Controller
     }
 
     /**
+     * Создание директории
      * @param Request $request
      * @return array|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
