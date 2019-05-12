50475b3da52e8a8ac939e79fa60b7e37d419cdb5
Roman Zakharov <camanru@ya.ru>
2019-05-12 16:16:11 +0300

    edit filemanager

--- src/views/filemanager/index.blade.php
+++ src/views/filemanager/index.blade.php
@@ -3,50 +3,31 @@
 @section('content')
 
     <div class="row">
-        @if(request()->input('folder'))
-            @php $getfolder = explode('/', request()->input('folder')); @endphp
-            @php array_pop($getfolder); @endphp
-            @php $getfolder = implode('/', $getfolder); @endphp
-            <a href="?folder={{ $getfolder }}&field={{ request()->input('field') }}">
-                <div class="block" style="background-image: url('/ico/folder.png');">
-                    <span class="filename">..</span>
+
+        <a href="?folder=&field={{ request()->input('field') }}">
+            <div class="block" style="background-image: url('/storage/fastleo/ico/folder.png');">
+                <span class="filename">..</span>
+            </div>
+        </a>
+
+        @foreach($folders as $folder)
+            <a href="?folder=&field={{ request()->input('field') }}">
+                <div class="block" style="background-image: url('/storage/fastleo/ico/folder.png');">
+                    <span class="filename">{{ $folder }}</span>
                 </div>
             </a>
-        @endif
-        @if(isset($folders) and count($folders) > 0)
-            @foreach($folders as $folder)
-                <a href="?folder=@if(request()->input('folder')){{ request()->input('folder') }}@endif/{{ $folder }}&field={{ request()->input('field') }}">
-                    <div class="block" style="background-image: url('/ico/folder.png');">
-                        <span class="filename">{{ $folder }}</span>
-                    </div>
-                </a>
-            @endforeach
-        @endif
-        @if(isset($files) and count($files) > 0)
-            @foreach($files as $file)
-                @php
-                    if(in_array(strtolower($file['ext']), $images) and $file['thumbs'] != '') {
-                        $background = $file['thumbs'];
-                    } else {
-                        $background = '/ico/'.$file['ext'].'.jpg';
-                    }
-                @endphp
-                <a href="">
-                    <div class="block image" style="background-image: url('{{ $background }}');" data-url="{{ $file['url'] }}">
+        @endforeach
+
+        @foreach($files as $file)
+            <a href="">
+                <div class="block image" style="background-image: url('/ico/jpg.jpg');" data-url="{{ $file['url'] }}">
                         <span class="filename">
-                            @php
-                                $len = strlen($file['name']);
-                            @endphp
-                            @if($len > 34)
-                                {{ str_limit($file['name'], 15, '...') }}{{ substr($file['name'], -18) }}
-                            @else
-                                {{ $file['name'] }}
-                            @endif
+                            {{ $file['name'] }}
                         </span>
-                    </div>
-                </a>
-            @endforeach
-        @endif
+                </div>
+            </a>
+        @endforeach
+        
     </div>
 
     @if(request()->input('field'))
