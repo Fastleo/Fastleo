0bc570745c7fc3ee60cd60b914c85c3b78389f26
Camanru <camanru@ya.ru>
2019-11-23 11:01:58 +0300

    2.0.9

--- src/views/rows/checkbox.blade.php
+++ src/views/rows/checkbox.blade.php
@@ -3,7 +3,7 @@
     <div class="col-sm-9">
         <div class="form-check">
             <input type="hidden" name="{{ $column }}" value="0">
-            <input type="checkbox" name="{{ $column }}" class="form-check-input" id="{{ $column }}" value="1" {{ $row->{$column} ? 'checked' : null }}>
+            <input type="checkbox" name="{{ $column }}" class="form-check-input" id="{{ $column }}" value="1" {{ (isset($row->{$column}) and $row->{$column} == 1) ? 'checked' : null }}>
         </div>
     </div>
 </div>
